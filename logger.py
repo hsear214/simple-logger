41815cf8fa539f228238ce5c434701a33c87680e
Humza Sear <humzasear2@gmail.com>
2026-07-06 12:01:21 +0100

    Initial commit: simple loggging program

---
+++ logger.py
@@ -0,0 +1,17 @@
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def main():
+    count = 0
+    while True:
+        count += 1
+        logging.info(f"Heartbeat log #{count}")
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
